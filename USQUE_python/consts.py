f129f6034b632f4a9d2a9370a70cf21fed776164
Tim Magoun <timmagoun@gmail.com>
2023-05-09 15:49:40 -0400

    added diff grades of sensors

--- USQUE_python/consts.py
+++ USQUE_python/consts.py
@@ -1,6 +1,8 @@
-# Parameters for fine-tuning filter. Crassidis and Markley recommmend
-#   a=1,lambda=1
-# Try (a,lambda) values (1,1), (1,0), (1,-1), and (2,-1)
+from numpy import pi
+
+# Type of IMU
+imu_grade = "mem"  # or "tac" or "nav"
+
 a = 1
 lam = 1
 f = 2 * (a + 1)
@@ -11,10 +13,26 @@ N = 10000  # Number of measurements
 n = 6
 g = 9.81  # m/s^2
 
-# Noise params for sensors
-sig_gy_b = 1e-4  # Gyro bias cov rad / s^(3/2)
-sig_gy_w = 2e-3  # Gyro white noise cov rad / s^(1/2)
-sig_acc = 1e-4  # Accel white noise cov m / s^(5/2)
+if imu_grade == "mem":
+    # Noise params for sensors
+    sig_gy_b = 30 / 60 / 180 * pi  # 30 deg/s^1/2
+    sig_gy_w = 0.2 / 60 / 180 * pi  # 0.2 deg / sqrt(hr)
+    sig_acc = 1e-4  # Accel white noise cov m / s / s^(1/2) aka acceleration random walk
+
+elif imu_grade == "tac":
+    # Noise params for sensors
+    sig_gy_b = 1.5 / 60 / 180 * pi  # 1.5 deg/s^1/2
+    sig_gy_w = 0.16 / 60 / 180 * pi  # 0.16 deg / sqrt(hr)
+    sig_acc = 1e-4  # Accel white noise cov m / s / s^(1/2) aka acceleration random walk
+
+elif imu_grade == "nav":
+    # Noise params for sensors
+    sig_gy_b = 6e-3 / 60 / 180 * pi  # 6e-3 deg/s^1/2
+    sig_gy_w = 3e-3 / 60 / 180 * pi  # 3e-3 deg / sqrt(hr)
+    sig_acc = 1e-4  # Accel white noise cov m / s / s^(1/2) aka acceleration random walk
+
+else:
+    raise ValueError(f"imu_grade = {imu_grade} is not valid")
 
 # Sampling frequency of the IMU
 fs = 100.0  # Hz
